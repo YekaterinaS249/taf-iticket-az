3e86cf6c346b8a91ebdb8a2657737f9667fb6b7d
yekaterina <katyatest97@gmail.com>
2026-05-23 03:03:28 +0400

    add annotation

--- src/main/java/az/iticket/api/model/LoginRequest.java
+++ src/main/java/az/iticket/api/model/LoginRequest.java
@@ -1,36 +1,18 @@
 package az.iticket.api.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class LoginRequest {
     private String email;
     private String password;
 
-    public LoginRequest() {
-
     }
-
-    public LoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
-
 
