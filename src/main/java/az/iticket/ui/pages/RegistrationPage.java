831cafd935a060345bda9d3034cad5f6def07372
yekaterina <katyatest97@gmail.com>
2026-06-03 03:17:27 +0400

    add methods

--- src/main/java/az/iticket/ui/pages/RegistrationPage.java
+++ src/main/java/az/iticket/ui/pages/RegistrationPage.java
@@ -1,11 +1,12 @@
 package az.iticket.ui.pages;
 
 import az.iticket.basepage.BasePage;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-
+@Slf4j
 public class RegistrationPage extends BasePage {
     private final String FIRST_NAME_INPUT = "//input[@name='first_name']";
     private final String LAST_NAME_INPUT = "//input[@name='last_name']";
@@ -19,6 +20,9 @@ public class RegistrationPage extends BasePage {
     private final String LOGIN_LINK = "//a[normalize-space()='Войдите здесь']";
     private final String CLOSE_BUTTON = "//h4[text()='Регистрация']/ancestor::div[contains(@class,'modal-dialog')]//button[contains(@class,'close')]";
     private final String CONFRIM_EMAIL = "//div[contains(text(),'подтвердите e-mail')]";
+    private final String EMPTY_FIRST_NAME_ERROR_MESSAGE = "//div[contains(text(),'Поле имя обязательно для заполнения')]";
+    private final String LONG_CREDENTIALS_ERROR_MESSAGE = "//div[contains(text(),'Количество символов в поле имя не может превышать 255')]";
+    private final String EMPTY_LAST_NAME_ERROR_MESSAGE = "//div[contains(text(),'Поле фамилия обязательно для заполнения')]";
 
     public RegistrationPage() {
         super();
@@ -32,15 +36,17 @@ public class RegistrationPage extends BasePage {
         return driver.findElement(By.xpath(REGISTRATION_FOOTER_TITTLE)).getText();
     }
 
-    public void fillRegistrationForm(String firstName, String lastName, String phoneNumber, String email,
-                                     String password, String confirmPassword) {
-        type(FIRST_NAME_INPUT,firstName);
-        type(LAST_NAME_INPUT,lastName);
-        type(PHONE_NUMBER_INPUT,phoneNumber);
-        type(EMAIL_INPUT,email);
-        type(PASSWORD_INPUT,password);
-        type(CONFIRM_PASSWORD_INPUT,confirmPassword);
+    public void fillRegistrationForm(String firstName, String lastName, String phoneNumber, String email, String password, String confirmPassword) {
+        log.info("Filling registration form with data: firstName = {} ,lastName = {},phoneNumber= {},email= {},password = {},confirmPassword = {}",
+                firstName, lastName, phoneNumber, email, password, confirmPassword);
+        type(FIRST_NAME_INPUT, firstName);
+        type(LAST_NAME_INPUT, lastName);
+        type(PHONE_NUMBER_INPUT, phoneNumber);
+        type(EMAIL_INPUT, email);
+        type(PASSWORD_INPUT, password);
+        type(CONFIRM_PASSWORD_INPUT, confirmPassword);
         click(REGISTRATION_BUTTON);
+        log.info("Clicking on the registration button");
     }
 
     public void type(String xpath, String text) {
@@ -62,6 +68,20 @@ public class RegistrationPage extends BasePage {
     public String getConfrimEmailMessage() {
         WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CONFRIM_EMAIL)));
         return email.getText();
+    }
+
+    public String getEmptyEmailErrorMessage() {
+        WebElement emptyInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(EMPTY_FIRST_NAME_ERROR_MESSAGE)));
+        return emptyInput.getText();
+    }
+
+    public String getLongCredentialsErrorMessage() {
+        WebElement longCredentials = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LONG_CREDENTIALS_ERROR_MESSAGE)));
+        return longCredentials.getText();
+    }
+
+    public String getEmptyLastNameErrorMessage() {
+        return driver.findElement(By.xpath(EMPTY_LAST_NAME_ERROR_MESSAGE)).getText();
     }
 
 }
--- src/test/java/az/iticket/ui/RegistrationTest.java
+++ src/test/java/az/iticket/ui/RegistrationTest.java
@@ -1,15 +1,19 @@
 package az.iticket.ui;
+
 import az.iticket.basetest.BaseTest;
 import az.iticket.ui.pages.AuthPage;
 import az.iticket.ui.pages.RegistrationPage;
+import az.iticket.utils.NegativeDataGenerator;
+import az.iticket.utils.PositiveDataGenerator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class RegistrationTest  extends BaseTest {
+public class RegistrationTest extends BaseTest {
     private AuthPage authPage;
     private RegistrationPage registrationPage;
+    private String password;
 
     @BeforeEach
     public void initRegistrationPage() {
@@ -17,16 +21,199 @@ public class RegistrationTest  extends BaseTest {
         authPage = new AuthPage();
         authPage.clickRegisterButton();
         registrationPage = new RegistrationPage();
+        password = PositiveDataGenerator.getPassword();
 
     }
+
     @DisplayName("Registration user with valid data")
     @Test
     public void registrationUserWithValidData() {
-        registrationPage.fillRegistrationForm("Yekaterina", "Kalin","507839039",
-                "y22888836@gmail.com","test1234","test1234");
-        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.",registrationPage.getConfrimEmailMessage());
+        registrationPage.fillRegistrationForm(
+                "Yekaterina",
+                "Kalin",
+                "507839039",
+                "y22888836@gmail.com",
+                "test1234",
+                "test1234");
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
     }
 
+    @DisplayName("Submit registration form with empty first name input")
+    @Test
+    public void submitRegistrationFormWithEmptyFirstName() {
+        registrationPage.fillRegistrationForm(
+                "",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Поле имя обязательно для заполнения.", registrationPage.getEmptyEmailErrorMessage());
+    }
 
+    @DisplayName("Sumbit registration form with cirilic first name")
+    @Test
+    public void submitRegistrationFormWithCirilicFirstName() {
+        registrationPage.fillRegistrationForm(
+                PositiveDataGenerator.getFirstNameRu(),
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+    }
 
+    @DisplayName("Submit registration form with hyphenated first name")
+    @Test
+    public void submitRegistrationFormWithHyphenatedFirstName() {
+        registrationPage.fillRegistrationForm(
+                "Anna-Maria",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+    }
+
+    @DisplayName("Submit regisration form with one-character first name")
+    @Test
+    public void submitRegistrationFormWithOneCharacterFirstName() {
+        registrationPage.fillRegistrationForm("K",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+    }
+
+    @DisplayName("Accept first name with 254 characters")
+    @Test
+    public void submitRegistrationFormWith254Characters() {
+        String firstName = PositiveDataGenerator.getFirstNameByLength(254);
+        registrationPage.fillRegistrationForm(firstName,
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+
+    }
+
+    @DisplayName("Accept first name with 255 characters")
+    @Test
+    public void submitRegistrationFormWith255Characters() {
+        String firstName = PositiveDataGenerator.getFirstNameByLength(255);
+        registrationPage.fillRegistrationForm(firstName,
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+    }
+
+    @DisplayName("Accept first name with 256 characters")
+    @Test
+    public void submitRegistrationFormWith256Characters() {
+        String firstName = NegativeDataGenerator.getFirstNameTooLong(256);
+        registrationPage.fillRegistrationForm(firstName,
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Количество символов в поле имя не может превышать 255.", registrationPage.getLongCredentialsErrorMessage());
+    }
+
+    //Система принимает спецсиволы в поле FirstName,регистрация проходит пользователь входит в систему,валидация отсутсвует.
+    @DisplayName("Accept first name with only symbols")
+    @Test
+    public void submitRegistrationFormWithSymbols() {
+        registrationPage.fillRegistrationForm("!@#$%^&*()",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+
+    }
+
+    //Система принимает только числа в поле FirstName,регистрация проходит пользователь входит в систему,валидация отсутвует.
+    @DisplayName("Accept first name with only digits")
+    @Test
+    public void submitRegistrationFormWithDigits() {
+        registrationPage.fillRegistrationForm("12345678910",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+    }
+
+    @DisplayName("Accept first name with leading space")
+    @Test
+    public void submitRegistrationFormWithLeadingSpace() {
+        registrationPage.fillRegistrationForm(" Yekaterina",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+    }
+
+    //Система сохраняет имя пользователя с пробелом по середине,возможно система поддерживает двойные имена.
+    @DisplayName("Submit registration form with space in first name")
+    @Test
+    public void submitRegistrationFormWithSpaceInFirstName() {
+        registrationPage.fillRegistrationForm("Anas tasiya",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+    }
+
+    @DisplayName("Submit registation form with first name ending spaces")
+    @Test
+    public void submitRegistrationFormWithFirstNameEndingSpaces() {
+        registrationPage.fillRegistrationForm("Yekaterina ",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Пожалуйста, подтвердите e-mail, чтобы продолжить пользоваться сайтом.", registrationPage.getConfrimEmailMessage());
+    }
+
+    @DisplayName("Submit registration form with first name only spaces ")
+    @Test
+    public void submitRegistrationFormWithOnlySpaces() {
+        registrationPage.fillRegistrationForm("         ",
+                PositiveDataGenerator.getLastName(),
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Поле имя обязательно для заполнения.", registrationPage.getEmptyEmailErrorMessage());
+    }
+
+    @DisplayName("Submit registration form with empty last name")
+    @Test
+    public void submitRegistrationFormWithEmptyLastName() {
+        registrationPage.fillRegistrationForm(PositiveDataGenerator.getFirstName(),
+                "",
+                PositiveDataGenerator.getPhoneNumberAz(),
+                PositiveDataGenerator.getEmail(),
+                password,
+                password);
+        Assertions.assertEquals("Поле фамилия обязательно для заполнения.",registrationPage.getEmptyLastNameErrorMessage());
+    }
 }
