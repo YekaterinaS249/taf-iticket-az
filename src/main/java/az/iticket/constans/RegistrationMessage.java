726f09a45d4f4dbc3eeb68747901bea23efeb952
yekaterina <katyatest97@gmail.com>
2026-06-12 03:11:16 +0400

    add constans message

--- src/main/java/az/iticket/constans/RegistrationMessage.java
+++ src/main/java/az/iticket/constans/RegistrationMessage.java
@@ -6,4 +6,12 @@ public class RegistrationMessage {
     public static final String LONG_FIRST_NAME_MESSAGE = "Количество символов в поле имя не может превышать 255.";
     public static final String EMPTY_LAST_NAME_INPUT_MESSAGE = "Поле фамилия обязательно для заполнения.";
     public static final String LONG_LAST_NAME_MESSAGE = "Количество символов в поле фамилия не может превышать 255.";
+    public static final String EMPTY_PHONE_NUMBER_ERROR_MESSAGE = "Поле моб. номер обязательно для заполнения.";
+    public static final String LONG_PHONE_NUMBER_ERROR_MESSAGE = "Количество символов в поле моб. номер не может превышать 255.";
+    public static final String EMPTY_EMAIL_ERROR_MESSAGE = "Поле e-mail адрес обязательно для заполнения.";
+    public static final String EMAIL_ALREADY_EXITS_ERROR_MESSAGE = "Такое значение поля e-mail адрес уже существует.";
+    public static final String INVALID_EMAIL_CREDENTIALS_MESSAGE = "Поле e-mail адрес должно быть действительным электронным адресом.";
+    public static final String EMPTY_PASSWORD_ERROR_MESSAGE = "Поле пароль обязательно для заполнения.";
+    public static final String SHORT_PASSWORD_ERROR_MESSAGE = "Количество символов в поле пароль должно быть не меньше 8.";
+    public static final String LONG_PASSWORD_ERROR_MESSAGE = "Количество символов в поле пароль не может превышать 255.";
 }
