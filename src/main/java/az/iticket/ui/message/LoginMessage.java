6a661419f4a6c5cbdfc2b7d4e248296b6eda56a0
yekaterina <katyatest97@gmail.com>
2026-06-18 02:50:31 +0400

    rename constans and login message

--- src/main/java/az/iticket/ui/message/LoginMessage.java
+++ src/main/java/az/iticket/ui/message/LoginMessage.java
@@ -1,9 +1,9 @@
 package az.iticket.ui.message;
 
 public class LoginMessage {
-    public static final String ERROR_EMPTY_EMAIL = "Поле e-mail адрес обязательно для заполнения.";
-    public static final String ERROR_EMPTY_PASSWORD = "Поле пароль обязательно для заполнения.";
-    public static final String INVALID_EMAIL_FORMAT = "Поле e-mail адрес должно быть действительным электронным адресом.";
+    public static final String EMAIL_REQUIRED_MESSAGE = "Email обязателен";
+    public static final String PASSWORD_INVALID_LENGTH_MESSAGE = "Пароль должен быть не менее 6 символов";
+    public static final String INVALID_EMAIL_ERROR_MESSAGE = "Неверный email";
     public static final String PASSWORD_TOO_SHORT = "Количество символов в поле пароль должно быть не меньше 8.";
     public static final String INVALID_CREDENTIALS = "The provided credentials do not match our records.";
     public static final String LOGIN_SUCCESS_MESSAGE = "Вы вошли в систему";
