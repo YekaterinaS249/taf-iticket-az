7a89da29e13332ca431e3f7d1ba25e81489ce31c
yekaterina <katyatest97@gmail.com>
2026-06-21 02:42:00 +0400

    refactor code and add test

--- src/test/java/az/iticket/ui/AuthFormTest.java
+++ src/test/java/az/iticket/ui/AuthFormTest.java
@@ -5,6 +5,9 @@ import az.iticket.ui.message.LoginMessage;
 import az.iticket.ui.pages.AuthPage;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 @Epic("Authentication")
@@ -20,8 +23,113 @@ public class AuthFormTest extends BaseTest {
         authPage = new AuthPage();
     }
 
-    @DisplayName("UI-LOG-001 -Verify validation error empty email")
-    @Story("Submit login form with empty email")
+    @DisplayName("Verify login with invalid password -UI-LOG-001")
+    @Story("Login validation")
+    @Test
+    public void loginWithWrongPassword() {
+        authPage.setInputEmail("omnqqmvgtlixqnwjtp@jbsze.com");
+        authPage.setInputPassword("test1234");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.INVALID_CREDENTIALS_ERROR_MESSAGE, authPage.getErrorInvalidCredentials());
+
+    }
+
+    @DisplayName("Verify login with non-registered user -UI-LOG-002")
+    @Story("Login validation")
+    @Test
+    public void verifyLoginWithWrongEmail() {
+        authPage.setInputEmail("silantyevakatya1@gmail.com");
+        authPage.setInputPassword("shelovespizza12");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.USER_NOT_FOUND_MESSAGE, authPage.getErrorMessageUserNotFound());
+    }
+
+    @DisplayName("Verify invalid email formats")
+    @Story("Email validation")
+    @ParameterizedTest(name = "Email: {0}")
+    @CsvSource({
+            "UI-LOG-003, '@gmail.com'",
+            "UI-LOG-004,'test@'",
+            "UI-LOG-005, 'user.user@@gmail.com'",
+            "UI-LOG-006,'user@.test.com'",
+            "UI-LOG-007, 'юзер@gmail.com'"
+    })
+    void verifyInvalidEmailTest(String testId, String email) {
+        authPage.setInputEmail(email);
+        authPage.setInputPassword("user1234");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.INVALID_EMAIL_ERROR_MESSAGE, authPage.getErrorInvalidEmail());
+
+    }
+
+    @DisplayName("Verify email validation with spaces")
+    @Story("Email validation")
+    @ParameterizedTest(name = "Email: {0}")
+    @CsvSource({
+            "UI-LOG-008, ' user@test.com'",
+            "UI-LOG-009, 'user @test.com'",
+            "UI-LOG-010, 'user@test.com '",
+            "UI-LOG-011, '      '"
+    })
+    void loginWithSpacesInEmailTest(String testId, String email) {
+        authPage.setInputEmail(email);
+        authPage.setInputPassword("user1234");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.INVALID_EMAIL_ERROR_MESSAGE, authPage.getErrorInvalidEmail());
+    }
+
+    @DisplayName("Verify validation invalid email format")
+    @Story("Email validation")
+    @ParameterizedTest(name = "Email: {0}")
+    @CsvSource({
+            "UI-LOG-012, 'user@test'",
+            "UI-LOG-013, 'user.@test.com'"
+    })
+    void loginWithInvalidEmailFormatTest(String testId, String email) {
+        authPage.setInputEmail(email);
+        authPage.setInputPassword("user1234");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.INVALID_FORMAT_EMAIL_MESSAGE, authPage.getErrorMessageInvalidEmailFormat());
+    }
+
+    @DisplayName("Verify email max length validation")
+    @Story("Email validation")
+    @ParameterizedTest(name = "UI-LOG-014, UI-LOG-015 {0}")
+    @ValueSource(ints = {254, 255})
+    void emailWithMaxAllowedLengthTest(int length) {
+        String email = "a".repeat(length - 10) + "@gmail.com";
+        authPage.setInputEmail(email);
+        authPage.setInputPassword("user1234");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.INVALID_FORMAT_EMAIL_MESSAGE, authPage.getErrorMessageInvalidEmailFormat());
+    }
+
+    @DisplayName("Verify email validation with tab and newline characters")
+    @Story("Email validation")
+    @ParameterizedTest(name = "UI-LOG-16, UI-LOG-017 {0}")
+    @CsvSource({
+            "UI-LOG-013,'user\\t@test.com",
+            "UI-LOG-014,'user\\n@test.com"
+    })
+    void emailWithTabAndNewLineTest(String testId, String email) {
+        authPage.setInputEmail(email);
+        authPage.setInputPassword("user1234");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.INVALID_EMAIL_ERROR_MESSAGE, authPage.getErrorInvalidEmail());
+    }
+
+    @DisplayName("Verify email validation with 256 characters -UI-LOG-018")
+    @Test
+    public void emailValidationWith256Characters() {
+        String email = "a".repeat(246) + "@gmail.com";
+        authPage.setInputEmail(email);
+        authPage.setInputPassword("user1234");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.MAX_LENGTH_EMAIL_ERROR_MESSAGE, authPage.getErrorMessageMaxLengthEmail());
+    }
+
+    @DisplayName("Verify empty email validation -UI-LOG-019")
+    @Story("Email validation")
     @Test
     public void loginWithEmptyEmailTest() {
         authPage.setInputEmail("");
@@ -30,34 +138,104 @@ public class AuthFormTest extends BaseTest {
         Assertions.assertEquals(LoginMessage.EMAIL_REQUIRED_MESSAGE, authPage.getErrorMessageEmptyInputEmail());
     }
 
-    @DisplayName("UI-LOG-002 -Verify validation error email without username")
-    @Story("Submit login form")
-    @Test
-    public void loginWithEmptyUsernameTest() {
-        authPage.setInputEmail("@gmail.com");
-        authPage.setInputPassword("user1234");
+    @DisplayName("Verify password validation by length")
+    @Story("Password validation")
+    @ParameterizedTest(name = "{0} | length = {1}")
+    @CsvSource({
+            "UI_LOG-020, 0",
+            "UI-LOG-021, 1",
+            "UI-LOG-022, 5"
+    })
+    void passwordValidationByLengthTest(String testId, int length) {
+        String password = "a".repeat(length);
+        authPage.setInputEmail("test@test.com");
+        authPage.setInputPassword(password);
         authPage.clickSubmitButton();
-        Assertions.assertEquals(LoginMessage.INVALID_EMAIL_ERROR_MESSAGE, authPage.getErrorInvalidEmail());
+        Assertions.assertEquals(LoginMessage.MIN_LENGTH_PASSWORD_ERROR_MESSAGE, authPage.getErrorLengthPassword());
+
     }
 
-    @DisplayName("UI-LOG-003 -Verify validation email without domain part")
-    @Story("Submit login form")
+    @DisplayName("Verify password validation error disappears after valid input -UI-LOG-023")
+    @Story("Password validation")
     @Test
-    public void loginWithEmptyDomainPartTest() {
-        authPage.setInputEmail("test");
-        authPage.setInputPassword("test1234");
+    public void passwordValidationErrorDisappearsAfterEntering6CharactersTest() {
+        authPage.setInputEmail("user@user.com");
+        authPage.setInputPassword("user12");
         authPage.clickSubmitButton();
-        Assertions.assertEquals(LoginMessage.INVALID_EMAIL_ERROR_MESSAGE, authPage.getErrorInvalidEmail());
+        Assertions.assertTrue(authPage.isPasswordValidationErrorNotDisplayed());
+
     }
 
-    @DisplayName("UI_LOG-005 -Verify validation error empty password")
-    @Story("Submit login form with empty password")
+    //В системе присутвует разночтение бэка и фронта на валидацию пароля.
+    @DisplayName("Verify password validation for 7-character password -UI-LOG-024")
+    @Story("Password validation")
     @Test
-    public void loginWithEmptyPasswordTest() {
-        authPage.setInputEmail("cqqgslqadspnhazzkz@vtmpj.com");
-        authPage.setInputPassword("");
+    public void passwordValidationFor7CharacterPasswordTest() {
+        authPage.setInputEmail("user@user.com");
+        authPage.setInputPassword("user123");
         authPage.clickSubmitButton();
-        Assertions.assertEquals(LoginMessage.PASSWORD_INVALID_LENGTH_MESSAGE, authPage.getErrorLengthPassword());
+        Assertions.assertEquals(LoginMessage.PASSWORD_TOO_SHORT, authPage.getErrorPasswordMinLength());
     }
 
+    @DisplayName("Verify password max length validation -UI-LOG-025")
+    @Story("Password validation")
+    @Test
+    public void passwordValidationByMaxLengthTest() {
+        String password = ("Aa1").repeat(85) + "A";
+        authPage.setInputEmail("user@user.com");
+        authPage.setInputPassword(password);
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.MAX_LENGTH_PASSWORD_ERROR_MESSAGE, authPage.getErrorPasswordMaxLength());
+    }
+
+    @DisplayName("Verify password with only spaces -UI-LOG-026")
+    @Story("Password validation")
+    @Test
+    public void passwordValidationWithOnlySpacesTest() {
+        authPage.setInputEmail("user@user.com");
+        authPage.setInputPassword("       ");
+        authPage.clickSubmitButton();
+        Assertions.assertEquals(LoginMessage.EMPTY_PASSWORD_ERROR_MESSAGE, authPage.getErrorMessageEmptyInputPassword());
+    }
+
+    @DisplayName("Verify auth page title -UI-LOG-027")
+    @Story("UI elements")
+    @Test
+    public void authPageTitleTest() {
+        Assertions.assertEquals("Войти", authPage.getAuthTitle());
+    }
+
+    @DisplayName("Verify auth page footer title -UI-LOG-028")
+    @Story("UI elements")
+    @Test
+    public void authPageFooterTitleTest() {
+        Assertions.assertEquals("Нет аккаунта?", authPage.getFooterAuthTitle());
+    }
+
+    @DisplayName("Verify email placeholder text -UI-LOG-029")
+    @Story("UI elements")
+    @Test
+    public void placeholderTextInEmailInputTest() {
+        Assertions.assertEquals("name@example.com", authPage.getEmailPlaceholder());
+
+    }
+
+    @DisplayName("Verify password visibility toggle -UI-LOG-030")
+    @Story("UI elements")
+    @Test
+    public void passwordVisibilityToggleTest() {
+        authPage.setInputPassword("12345678");
+        Assertions.assertEquals("password", authPage.getPasswordFieldType());
+        authPage.clickTogglePasswordButton();
+        Assertions.assertEquals("text", authPage.getPasswordFieldType());
+    }
+
+    @DisplayName("Verify login modal closes correctly -UI-LOG-031")
+    @Story("UI elements")
+    @Test
+    public void modalWindowLoginNotDisplayedTest() {
+        authPage.clickCloseButton();
+        Assertions.assertTrue(authPage.isModalLoginInvisible());
+
+    }
 }
