fbec5e9b6fb84fafa90ca6d280f212ab4b5aa3be
yekaterina <katyatest97@gmail.com>
2026-06-21 03:04:31 +0400

    refactor error message text

--- src/main/java/az/iticket/ui/message/LoginMessage.java
+++ src/main/java/az/iticket/ui/message/LoginMessage.java
@@ -2,9 +2,14 @@ package az.iticket.ui.message;
 
 public class LoginMessage {
     public static final String EMAIL_REQUIRED_MESSAGE = "Email обязателен";
-    public static final String PASSWORD_INVALID_LENGTH_MESSAGE = "Пароль должен быть не менее 6 символов";
     public static final String INVALID_EMAIL_ERROR_MESSAGE = "Неверный email";
+    public static final String INVALID_FORMAT_EMAIL_MESSAGE = "Поле e-mail адрес должно быть действительным электронным адресом.";
+    public static final String USER_NOT_FOUND_MESSAGE = "Пользователь не найден";
+    public static final String INVALID_CREDENTIALS_ERROR_MESSAGE = "Неверное имя пользователя или пароль.";
+    public static final String MIN_LENGTH_PASSWORD_ERROR_MESSAGE = "Пароль должен быть не менее 6 символов";
+    public static final String MAX_LENGTH_PASSWORD_ERROR_MESSAGE = "Количество символов в поле пароль не может превышать 255.";
+    public static final String EMPTY_PASSWORD_ERROR_MESSAGE = "Поле пароль обязательно для заполнения.";
+    public static final String MAX_LENGTH_EMAIL_ERROR_MESSAGE = "Количество символов в поле e-mail адрес не может превышать 255.";
     public static final String PASSWORD_TOO_SHORT = "Количество символов в поле пароль должно быть не меньше 8.";
-    public static final String INVALID_CREDENTIALS = "The provided credentials do not match our records.";
-    public static final String LOGIN_SUCCESS_MESSAGE = "Вы вошли в систему";
+
 }
