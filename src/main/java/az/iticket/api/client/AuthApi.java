d00eb18a034338a1848c0895351255a15432fb53
yekaterina <katyatest97@gmail.com>
2026-06-22 02:58:34 +0400

    add logs SLF4j

--- src/main/java/az/iticket/api/client/AuthApi.java
+++ src/main/java/az/iticket/api/client/AuthApi.java
@@ -2,16 +2,25 @@ package az.iticket.api.client;
 
 import az.iticket.api.endpoints.Endpoints;
 import az.iticket.api.model.LoginRequest;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+
 import static io.restassured.RestAssured.given;
 
+@Slf4j
 public class AuthApi {
     public static Response login(LoginRequest request) {
-        return  given()
+        log.info("Login request: {}", request.toString());
+        Response response =  given()
+                .filter(new AllureRestAssured())
                 .contentType(ContentType.JSON)
                 .body(request)
                 .when()
                 .post(Endpoints.LOGIN);
+
+        log.info("Login response status code: {}", response.getStatusCode());
+        return response;
     }
 }
