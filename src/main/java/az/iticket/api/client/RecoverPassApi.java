db442866a02e81833d43ecaf37733e84f7b3227a
yekaterina <katyatest97@gmail.com>
2026-06-22 03:05:06 +0400

    add annotation lombok

--- src/main/java/az/iticket/api/client/RecoverPassApi.java
+++ src/main/java/az/iticket/api/client/RecoverPassApi.java
@@ -2,18 +2,27 @@ package az.iticket.api.client;
 
 import az.iticket.api.endpoints.Endpoints;
 import az.iticket.api.model.RecoverPassRequest;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+
 import static io.restassured.RestAssured.given;
 
-
+@Slf4j
 public class RecoverPassApi {
-    public static Response recoverPass(RecoverPassRequest passRequest) {
-        return given()
+    public static Response recoverPassword(RecoverPassRequest passRequest) {
+        log.info("RecoverPass Request: {}", passRequest.toString());
+        Response response= given()
+                .filter(new AllureRestAssured())
                 .contentType(ContentType.JSON)
                 .body(passRequest)
                 .when()
                 .post(Endpoints.RECOVER_PASSWORD);
 
+        log.info("RecoverPass Response status code: {}", response.getStatusCode());
+        return response;
     }
 }
+
+
