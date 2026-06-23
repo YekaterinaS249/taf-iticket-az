c1b07bf7e5b24ee19815aeb8923147377c008b00
yekaterina <katyatest97@gmail.com>
2026-06-24 03:09:32 +0400

    reformat code

--- src/main/java/az/iticket/ui/core/DriverManager.java
+++ src/main/java/az/iticket/ui/core/DriverManager.java
@@ -7,14 +7,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 @Slf4j
 public class DriverManager {
     private static WebDriver driver;
+
     public static WebDriver getDriver() {
         if (driver == null) {
             log.info("Browser is open");
             driver = new ChromeDriver();
             driver.manage().window().maximize();
         }
-        return  driver;
+        return driver;
     }
+
     public static void quitDriver() {
         if (driver != null) {
             log.info("Browser is quitting");
