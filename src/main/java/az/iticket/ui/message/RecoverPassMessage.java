effbedd7d807f0713f42b8179b5a7e60b8726548
yekaterina <katyatest97@gmail.com>
2026-06-23 23:34:03 +0400

    refactor message

--- src/main/java/az/iticket/ui/message/RecoverPassMessage.java
+++ src/main/java/az/iticket/ui/message/RecoverPassMessage.java
@@ -1,8 +1,10 @@
 package az.iticket.ui.message;
 
 public class RecoverPassMessage {
-    public static final String RESET_LINK_SENT_MESSAGE = "Ссылка на сброс пароля была отправлена!";
-    public static final String EMPTY_EMAIL_MESSAGE = "Поле e-mail адрес обязательно для заполнения.";
-    public static final String INVALID_EMAIL_CREDENTIALS = "Поле e-mail адрес должно быть действительным электронным адресом.";
-    public static final String LONG_EMAIL_ERROR_MESSAGE = "Количество символов в поле e-mail адрес не может превышать 255.";
+    public static final String RESET_LINK_SENT_MESSAGE = "Если аккаунт с указанным адресом электронной почты существует, ссылка для сброса пароля была отправлена.";
+    public static final String EMAIL_REQUIRED_ERROR_MESSAGE = "Email обязателен";
+    public static final String INVALID_EMAIL_ERROR_MESSAGE = "Неверный email";
+    public static final String INVALID_FORMAT_EMAIL_ERROR_MESSAGE = "Поле e-mail адрес должно быть действительным электронным адресом.";
+    public static final String MAX_LENGTH_EMAIL_ERROR_MESSAGE = "Количество символов в поле e-mail адрес не может превышать 255.";
+    public static final String RESET_PASSWORD_MESSAGE = "Введите email, мы отправим ссылку для сброса пароля";
 }
