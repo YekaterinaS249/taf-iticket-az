bc46c1ede3817e1f1e6d53f040b27842f24486a1
yekaterina <katyatest97@gmail.com>
2026-07-01 02:55:43 +0400

    add logging and step annotation

--- src/main/java/az/iticket/ui/pages/EventPage.java
+++ src/main/java/az/iticket/ui/pages/EventPage.java
@@ -1,12 +1,16 @@
 package az.iticket.ui.pages;
+
 import az.iticket.ui.pages.basepage.BasePage;
+import io.qameta.allure.Step;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Slf4j
 public class EventPage extends BasePage {
     private final String SESSION_PRICE = "//span[contains(@class,'session-tile__price')]";
     private final String FIRST_SESSION_PRICE = "(//span[contains(@class,'session-tile__price')])[1]";
@@ -16,21 +20,30 @@ public class EventPage extends BasePage {
 
     }
 
+    @Step("Get prices of all sessions")
     public List<String> getAllSessionPrice() {
-        return driver.findElements(By.xpath(SESSION_PRICE))
+        List<String> prices = driver.findElements(By.xpath(SESSION_PRICE))
                 .stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
+        log.info("Session prices found: {}", prices);
+        return prices;
     }
 
+    @Step("Get price of first session")
     public String getFirstSessionPrice() {
-        return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(FIRST_SESSION_PRICE))).getText();
+        String price = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(FIRST_SESSION_PRICE))).getText();
+        log.info("Session price found: {}", price);
+        return price;
     }
 
+    @Step("Check first session price is displayed")
     public boolean isFirstSessionPriceDisplayed() {
-        return  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(FIRST_SESSION_PRICE))).isDisplayed();
-
+       boolean price =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(FIRST_SESSION_PRICE))).isDisplayed();
+       log.info("Session price found: {}", price);
+        return price;
     }
 }
+
 
 
