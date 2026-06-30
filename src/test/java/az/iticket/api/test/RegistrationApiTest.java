2cdf6dffe8058a0c781010076dabcd2f01840dc6
yekaterina <katyatest97@gmail.com>
2026-07-01 02:57:47 +0400

    refactor code

--- src/test/java/az/iticket/api/test/RegistrationApiTest.java
+++ src/test/java/az/iticket/api/test/RegistrationApiTest.java
@@ -2,6 +2,7 @@ package az.iticket.api.test;
 
 import az.iticket.api.client.RegistrationApi;
 import az.iticket.api.data.RegistrationDataFactory;
+import az.iticket.api.message.RegistrationApiMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,39 +12,40 @@ public class RegistrationApiTest {
 
     @DisplayName("Verify short phone number -API-REG-001")
     @Test
-     public void registerPhoneNumber() {
+    public void registerPhoneNumber() {
         RegistrationApi.registration(RegistrationDataFactory.getShortPhoneNumber())
                 .then()
                 .statusCode(200)
-                .body("response.email_verification.message",equalTo("Пожалуйста, проверьте свою электронную почту для получения кода подтверждения"));
+                .body("response.email_verification.message", equalTo(RegistrationApiMessage.EMAIL_CONFIRM_MESSAGE));
 
     }
 
     @DisplayName("Verify max length number -API-REG-002")
     @Test
-     public void registerMaxLengthNumber() {
+    public void registerMaxLengthNumber() {
         RegistrationApi.registration(RegistrationDataFactory.getMaxLengthPhoneNumber())
                 .then()
                 .statusCode(200)
-                .body("response.email_verification.message",equalTo("Пожалуйста, проверьте свою электронную почту для получения кода подтверждения"));
+                .body("response.email_verification.message", equalTo(RegistrationApiMessage.EMAIL_CONFIRM_MESSAGE));
     }
 
     @DisplayName("Verify phone number only letters -API-REG-003")
     @Test
-     public void registerPhoneNumberOnlyLetters() {
+    public void registerPhoneNumberOnlyLetters() {
         RegistrationApi.registration(RegistrationDataFactory.getPhoneNumberFieldRejectsLetters())
                 .then()
                 .statusCode(200)
-                .body("response.email_verification.message",equalTo("Пожалуйста, проверьте свою электронную почту для получения кода подтверждения"));
+                .body("response.email_verification.message", equalTo(RegistrationApiMessage.EMAIL_CONFIRM_MESSAGE));
     }
 
     @DisplayName("Verify phone number only symbols -API-REG-004")
     @Test
-     public void registerPhoneNumberOnlySymbols() {
+    public void registerPhoneNumberOnlySymbols() {
         RegistrationApi.registration(RegistrationDataFactory.getPhoneNumberFieldRejectsOnlySymbols())
                 .then()
                 .statusCode(200)
-                .body("response.email_verification.message",equalTo("Пожалуйста, проверьте свою электронную почту для получения кода подтверждения"));
+                .body("response.email_verification.message", equalTo(RegistrationApiMessage.EMAIL_CONFIRM_MESSAGE));
     }
 }
+
 
