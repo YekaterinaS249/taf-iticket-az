78b9b9be596c42a44cecf599d4d17ed59673dfa9
yekaterina <katyatest97@gmail.com>
2026-07-01 02:57:29 +0400

    add RegistrationApiMessage class

---
+++ src/main/java/az/iticket/api/message/RegistrationApiMessage.java
@@ -0,0 +1,5 @@
+package az.iticket.api.message;
+
+public class RegistrationApiMessage {
+    public static final String EMAIL_CONFIRM_MESSAGE = "Пожалуйста, проверьте свою электронную почту для получения кода подтверждения";
+}
