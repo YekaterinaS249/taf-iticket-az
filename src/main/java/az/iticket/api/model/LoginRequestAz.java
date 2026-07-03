4e3ecd2db5d466807ad1bc1b546f17cfc07c0758
yekaterina <katyatest97@gmail.com>
2026-07-04 01:53:53 +0400

    add LoginRequestAz class

---
+++ src/main/java/az/iticket/api/model/LoginRequestAz.java
@@ -0,0 +1,16 @@
+package az.iticket.api.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LoginRequestAz {
+    String email;
+    String password;
+}
+
