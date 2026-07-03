93a1c50e1109a700e2d91967b8a9aaa4c1e10349
yekaterina <katyatest97@gmail.com>
2026-07-04 01:50:44 +0400

    add class EndpointsAz

---
+++ src/main/java/az/iticket/api/endpoints/EndpointsAz.java
@@ -0,0 +1,10 @@
+package az.iticket.api.endpoints;
+
+public class EndpointsAz {
+    public static final String BASE_URL =
+            "https://api.iticket.az/az";
+
+    public static final String LOGIN =
+            BASE_URL + "/v6/user/auth/token?client=web";
+
+}
