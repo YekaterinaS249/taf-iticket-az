725f9d4227eaa668895555c80876fef04dedf7fe
yekaterina <katyatest97@gmail.com>
2026-07-04 00:30:44 +0400

    add enum class for recover password

--- src/main/java/az/iticket/ui/message/RecoverPassMessage.java
+++ src/main/java/az/iticket/ui/message/RecoverPassMessage.java
@@ -1,10 +1,48 @@
 package az.iticket.ui.message;
 
-public class RecoverPassMessage {
-    public static final String RESET_LINK_SENT_MESSAGE = "Если аккаунт с указанным адресом электронной почты существует, ссылка для сброса пароля была отправлена.";
-    public static final String EMAIL_REQUIRED_ERROR_MESSAGE = "Email обязателен";
-    public static final String INVALID_EMAIL_ERROR_MESSAGE = "Неверный email";
-    public static final String INVALID_FORMAT_EMAIL_ERROR_MESSAGE = "Поле e-mail адрес должно быть действительным электронным адресом.";
-    public static final String MAX_LENGTH_EMAIL_ERROR_MESSAGE = "Количество символов в поле e-mail адрес не может превышать 255.";
-    public static final String RESET_PASSWORD_MESSAGE = "Введите email, мы отправим ссылку для сброса пароля";
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+public enum RecoverPassMessage {
+    RESET_LINK_SENT("reset.link.sent.message"),
+    EMAIL_REQUIRED("email.required.error.message"),
+    INVALID_EMAIL("invalid.email.error.message"),
+    INVALID_EMAIL_FORMAT("invalid.format.email.error.message"),
+    MAX_EMAIL_LENGTH("max.length.email.error.message"),
+    RESET_PASSWORD("reset.password.message");
+
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        try (InputStream input = RecoverPassMessage.class
+                .getClassLoader()
+                .getResourceAsStream("recover.password.ui_ru.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("Properties file not found: recover.password.ui_ru.properties");
+            }
+
+            try (InputStreamReader reader =
+                         new InputStreamReader(input, StandardCharsets.UTF_8)) {
+                PROPERTIES.load(reader);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load properties file", e);
+        }
+    }
+
+    private final String key;
+
+    RecoverPassMessage(String key) {
+        this.key = key;
+    }
+
+    public String getMessage() {
+        return PROPERTIES.getProperty(key);
+    }
 }
