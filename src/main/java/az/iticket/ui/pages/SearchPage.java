49d277afc9d85f5a72e3a075546616594b68c932
yekaterina <katyatest97@gmail.com>
2026-07-05 03:51:31 +0400

    refactor code

--- src/main/java/az/iticket/ui/pages/SearchPage.java
+++ src/main/java/az/iticket/ui/pages/SearchPage.java
@@ -27,121 +27,120 @@ public class SearchPage extends BasePage {
 
     @Step("Enter '{searchInput}' into search input")
     public void setSearchInput(String searchInput) {
-        WebElement searchInputElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(SEARCH_INPUT)));
-        log.info("Entering '{}' into search input" , searchInput);
+        WebElement searchInputElement = waitForVisibility(SEARCH_INPUT);
+        log.info("Entering '{}' into search input", searchInput);
         searchInputElement.sendKeys(searchInput);
     }
 
     @Step("Clear search input")
     public void clearSearchInput() {
-        driver.findElement(By.xpath(SEARCH_INPUT)).clear();
+        waitForVisibility(SEARCH_INPUT).clear();
         log.info("Clearing search input");
     }
 
     @Step("Get search input placeholder")
     public String getSearchInputPlaceholder() {
-        String placeholder = driver.findElement(By.xpath(SEARCH_INPUT)).getAttribute("placeholder");
+        String placeholder = waitForVisibility(SEARCH_INPUT).getAttribute("placeholder");
         log.info("Search input placeholder: '{}'", placeholder);
         return placeholder;
     }
 
     @Step("Get search input value")
     public String getSearchInputValue() {
-        String value = driver.findElement(By.xpath(SEARCH_INPUT)).getAttribute("value");
+        String value = waitForVisibility(SEARCH_INPUT).getAttribute("value");
         log.info("Search input value: '{}'", value);
         return value;
     }
 
     @Step("Click outside the search dropdown")
-    public void clickOutsideDropdown(){
+    public void clickOutsideDropdown() {
         driver.findElement(By.tagName("body")).click();
         log.info("Clicking outside dropdown");
     }
 
-    @Step("Check if search input is displayed")
+    @Step("Verify search input is displayed")
     public boolean isSearchInputDisplayed() {
-        boolean isDisplayed = driver.findElement(By.xpath(SEARCH_INPUT)).isDisplayed();
+        boolean isDisplayed = waitForVisibility(SEARCH_INPUT).isDisplayed();
         log.info("Search input isDisplayed: '{}'", isDisplayed);
         return isDisplayed;
     }
 
     @Step("Click search icon")
     public void clickSearchIcon() {
-        driver.findElement(By.xpath(SEARCH_ICON)).click();
+        click(SEARCH_ICON);
         log.info("Clicking search icon");
     }
 
-    @Step("Check if search icon is displayed")
+    @Step("Verify search icon is displayed")
     public boolean isSearchIconDisplayed() {
-        boolean isDisplayed = driver.findElement(By.xpath(SEARCH_ICON)).isDisplayed();
+        boolean isDisplayed = waitForVisibility(SEARCH_ICON).isDisplayed();
         log.info("Search icon isDisplayed: '{}'", isDisplayed);
         return isDisplayed;
     }
 
-    @Step("Check if search dropdown is displayed")
+    @Step("Verify search dropdown is displayed")
     public boolean isSearchDropdownDisplayed() {
-        boolean isDisplayed = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(SEARCH_DROPDOWN))).isDisplayed();
+        boolean isDisplayed = waitForVisibility(SEARCH_DROPDOWN).isDisplayed();
         log.info("Search dropdown isDisplayed: '{}'", isDisplayed);
         return isDisplayed;
     }
 
-    @Step("Check if search dropdown is closed")
+    @Step("Verify search dropdown is closed")
     public boolean isSearchDropdownClosed() {
         boolean dropdownClosed = wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(SEARCH_DROPDOWN)));
         log.info("Search dropdown isClosed: '{}'", dropdownClosed);
         return dropdownClosed;
     }
 
-    @Step("Check if 'Top  event' header is displayed")
+    @Step("Verify Top Event header is displayed")
     public boolean isTopEventHeaderDisplayed() {
-        boolean eventHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(TOP_EVENT_HEADER))).isDisplayed();
+        boolean eventHeader = waitForVisibility(TOP_EVENT_HEADER).isDisplayed();
         log.info("Top event header isDisplayed: '{}'", eventHeader);
         return eventHeader;
     }
 
     @Step("Get 'Top event' header text")
     public String getTopEventHeaderText() {
-        String text = driver.findElement(By.xpath(TOP_EVENT_HEADER)).getText();
+        String text = waitForVisibility(TOP_EVENT_HEADER).getText();
         log.info("Top event header text: '{}'", text);
         return text;
     }
 
-    @Step("Check if 'Top place' header is displayed")
+    @Step("Verify Top Places header is displayed")
     public boolean isTopPlaceHeaderDisplayed() {
-        boolean isDisplayed = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(TOP_PLACES_HEADER))).isDisplayed();
+        boolean isDisplayed = waitForVisibility(TOP_PLACES_HEADER).isDisplayed();
         log.info("Top place header isDisplayed: '{}'", isDisplayed);
         return isDisplayed;
     }
 
     @Step("Get 'Top place' header text")
     public String getTopPlaceHeaderText() {
-        String text = driver.findElement(By.xpath(TOP_PLACES_HEADER)).getText();
+        String text = waitForVisibility(TOP_PLACES_HEADER).getText();
         log.info("Top place header text: '{}'", text);
         return text;
     }
 
-    @Step("Get list of event items")
+    @Step("Get event items")
     public List<WebElement> getEventItems() {
-        List<WebElement> items = driver.findElements(By.xpath(EVENT_ITEMS));
+        List<WebElement> items = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(EVENT_ITEMS)));
         log.info("Found {} event items", items.size());
         return items;
     }
 
     @Step("Click first event item")
     public void clickFirstEventItem() {
-        WebElement firstEvent = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(EVENT_ITEMS)));
-        log.info("First event item is clicked: '{}'", firstEvent);
-        firstEvent.click();
+        click(EVENT_ITEMS);
+        log.info("User clicked first event item");
     }
 
-    @Step("Check  URL changed to '{expectedUrl}'")
+    @Step("Verify URL is '{expectedUrl}'")
     public boolean isUrlChanged(String expectedUrl) {
-        boolean result = wait.until(ExpectedConditions.urlToBe(expectedUrl));
-        log.info("Url changed to expected value: '{}'", result);
-        return result;
+        boolean isUrlChanged = wait.until(ExpectedConditions.urlToBe(expectedUrl));
+        log.info("URL changed to '{}': {}", expectedUrl, isUrlChanged);
+        return isUrlChanged;
     }
 
-    @Step("Check  URL changed to venue page '{expectedUrl}'")
+    @Step("Verify URL is '{expectedUrl}''")
     public boolean isUrlChangedToVenue(String expectedUrl) {
         boolean result = wait.until(ExpectedConditions.urlToBe(expectedUrl));
         log.info("URL changed to venue page: {}", result);
@@ -157,17 +156,17 @@ public class SearchPage extends BasePage {
 
     @Step("Get texts of all event items")
     public List<String> getEventItemText() {
-        List<String> texts = driver.findElements(By.xpath(EVENT_ITEMS))
+        List<String> texts = getEventItems()
                 .stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
-        log.info("Event item texts: {}" , texts);
+        log.info("Event item texts: {}", texts);
         return texts;
     }
 
     @Step("Get href of first event item")
     public String getFirstEventItemsHref() {
-        String href = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(EVENT_ITEMS))).getAttribute("href");
+        String href = waitForVisibility(EVENT_ITEMS).getAttribute("href");
         log.info("First event item href: '{}'", href);
         return href;
     }
@@ -175,16 +174,17 @@ public class SearchPage extends BasePage {
     @Step("Click first place item")
     public void clickFirstPlaceItem() {
         log.info("Clicking first place item");
-        driver.findElement(By.xpath(PLACE_ITEMS)).click();
+        click(PLACE_ITEMS);
     }
 
     @Step("Get href of first place item")
     public String getFirstPlaceItemsHref() {
-        String href = driver.findElement(By.xpath(PLACE_ITEMS)).getAttribute("href");
+        String href = waitForVisibility(PLACE_ITEMS).getAttribute("href");
         log.info("First place item href: '{}'", href);
         return href;
 
     }
 }
+
 
 
